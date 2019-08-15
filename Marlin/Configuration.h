9d57acd5f73908f7b923190e5a9b7f8d2be43326
Florian Tales <florian.tales@arkea.com>
2019-08-15 11:57:23 +0200

    Ajustement taille plateau - limites - offset milieu

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -881,13 +881,13 @@
 
 // The size of the print bed
 // Obligation de reduire : vitre + pinces
-#define X_BED_SIZE 150
-#define Y_BED_SIZE 150
+#define X_BED_SIZE 180
+#define Y_BED_SIZE 170
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 // On repositionne les limites du plateau par rapport aux endstops
-#define X_MIN_POS -50
-#define Y_MIN_POS -35
+#define X_MIN_POS -18
+#define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
@@ -1132,8 +1132,8 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-//#define MANUAL_X_HOME_POS 0
-//#define MANUAL_Y_HOME_POS 0
+#define MANUAL_X_HOME_POS -20
+#define MANUAL_Y_HOME_POS -22
 //#define MANUAL_Z_HOME_POS 0
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
