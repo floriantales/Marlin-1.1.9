6171b17b60030521c4860d59a7bc2cab657f6deb
Florian Tales <florian.tales@arkea.com>
2021-11-14 14:16:11 +0100

    Too much thermal protection runaway

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -778,8 +778,8 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -24  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 0  // Y offset: -front +behind [the nozzle]
-// Pour se rapprocher du plateau il faut augmenter :
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.9   // Z offset: -below +above  [the nozzle]
+// Pour se rapprocher du plateau il faut diminuer (-2 est plus proche du plateau que -1) :
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.4   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
